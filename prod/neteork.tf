ac535126b617e6831daccf2e58fc14ef15f0a098
kokinx <yagitanikoki@gmail.com>
2023-05-21 16:05:37 +0900

    変数修正

--- prod/neteork.tf
+++ prod/neteork.tf
@@ -2,7 +2,7 @@ module "network" {
     source = "../modules/network"
 
     project = var.project
-    env = var.project
+    env = var.env
     vpc_cidr = "10.0.0.0/16"
     public_subnets = var.public_subnets
     private_subnets = var.private_subnets
